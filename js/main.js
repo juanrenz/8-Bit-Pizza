a7d791209c62a866b23972f6fac2e1439e742e5a
juanRenz <juanrenz@gmail.com>
2021-09-23 23:10:33 -0300

    Metodo para agregar cards con ingredientes de manera dinamica mendiante DOM

--- js/main.js
+++ js/main.js
@@ -15,26 +15,23 @@ productos.push(new Producto("2", "Cebolla", 30, "Cebolla a la plancha"));
 productos.push(new Producto("3", "Salsa", 20, "Elige tu preferida!"));
 productos.push(new Producto("4", "Anchoas", 100, "Anchoas sobre tu pizza"));
 
+let contenedor = document.getElementById("principal")
+let acumulador = "";
 
 
 for (let i = 0; i < productos.length; i++) {
 
-    let contenedor = document.createElement("div");
-    contenedor.className = "principal__cards"
-
-    contenedor.innerHTML += `<div class="principal__cards__contenedor">
-                                <div class="card" style="width: 18rem;">
-                                    <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwy1aUOF5yO-8x5JhprUk2Oe9aPkUEZKf5VLka8cyS6v14XtpyZfo8EPSTwH3YQPa68Lk&usqp=CAU" alt="Card image cap">
-                                    <div class="card-body">
-                                        <h3> Producto: ${productos[i].nombre} </h3>
-                                        <p> ${productos[i].caracteristica} </p>
-                                        <b> $ ${productos[i].precio} </b>;
-                                    </div>
-                                </div>
-                            </div>`;
-
-    var prueba = document.getElementsByTagName("section")[i];
-    console.log(prueba)
-    prueba.appendChild(contenedor)
+    acumulador += `<div class="principal__cards__contenedor">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwy1aUOF5yO-8x5JhprUk2Oe9aPkUEZKf5VLka8cyS6v14XtpyZfo8EPSTwH3YQPa68Lk&usqp=CAU" alt="Card image cap">
+                        <div class="card-body">
+                            <h3> Producto: ${productos[i].nombre} </h3>
+                            <p> ${productos[i].caracteristica} </p>
+                            <b> $ ${productos[i].precio} </b>;
+                        </div>
+                    </div>
+                </div>`;
 }
+
+contenedor.innerHTML = acumulador;
 
