aed319ce0f9456179d11388e46fede1ba0348a01
juanRenz <juanrenz@gmail.com>
2021-10-16 14:25:51 -0300

    AJAX

--- js/ingredientes.js
+++ js/ingredientes.js
@@ -1,30 +1,42 @@
-const JSONIngredientes = "data/datos.json"
+class Producto {
+    constructor(id, nombre, precio, caracteristica, img, cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.caracteristica = caracteristica;
+        this.img = img;
+        this.cantidad = cantidad;
+    }
+}
+
 const productos = [];
+const JSONIngredientes = "data/datos.json";
+
+$.getJSON(JSONIngredientes, function (respuesta, estado){
+    let ingredientes = respuesta;
+    
+    if(estado === "success"){
+        for (const ingrediente of ingredientes){
+            productos.push(new Producto (ingrediente.id, ingrediente.nombre, ingrediente.precio, ingrediente.caracteristica, ingrediente.img, ingrediente.cantidad))
+        }
+    }
+});
+
 const guardarStorage = (clave, valor) => { localStorage.setItem(clave, valor) };
 
-$.getJSON(JSONIngredientes, function (respuesta, estado) {
-    if(estado === "success"){
+guardarStorage("ingredientes", JSON.stringify(productos));
 
-        let ingredientes = respuesta
+for (let i = 0; i < productos.length; i++) {
 
-        for (let i = 0; i < ingredientes.length; i++) {
-            $("#principal").prepend(`<div class="principal__cards__contenedor">
-                                        <div class="card" style="width: 23rem;">
-                                            <img class="card-img-top" src="${ingredientes[i].img}" alt="Card image cap">
-                                            <div class="card-body">
-                                                <h3> ${ingredientes[i].nombre} </h3>
-                                                <p> ${ingredientes[i].caracteristica} </p>
-                                                <b> $ ${ingredientes[i].precio} </b>
-                                            </div>
-                                            <button id=boton${ingredientes[i].id} type="button" class="btn btn-outline-warning ">Agregar</button>
-                                        </div>
-                                    </div>`);
-
-            productos.push(ingredientes[i]);
-        }
-        guardarStorage("ingredientes", JSON.stringify(productos));
-    }
-})
-
-
-
+    $("#principal").prepend( `<div class="principal__cards__contenedor">
+                    <div class="card" style="width: 23rem;">
+                        <img class="card-img-top" src="${productos[i].img}" alt="Card image cap">
+                        <div class="card-body">
+                            <h3> ${productos[i].nombre} </h3>
+                            <p> ${productos[i].caracteristica} </p>
+                            <b> $ ${productos[i].precio} </b>
+                        </div>
+                        <button id=boton${productos[i].id} type="button" class="btn btn-outline-warning ">Agregar</button>
+                    </div>
+                </div>`);
+}
